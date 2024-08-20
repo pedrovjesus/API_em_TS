5136011a70ae7e9f91eaa134294ddccc3fed566d
pedrovjesus <pedroferreira161jesus@gmail.com>
2024-08-20 20:21:23 -0300

    feat: adicionado config de ambiente para produção

--- src/server/database/knex/index.ts
+++ src/server/database/knex/index.ts
@@ -1,7 +1,13 @@
 import knex from "knex";
+import pg from "pg";
+import 'dotenv/config'
 import { development, production, test } from "./Environment";
 
 const getEnvironment = () => {
+  if (process.env.NODE_ENV === "production") {
+    pg.types.setTypeParser(20, "text", parseInt);
+  }
+
   switch (process.env.NODE_ENV) {
     case "production":
       return production;
